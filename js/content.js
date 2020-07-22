81d158dda9eeedd5085d0fa4d2d17015eaaa4e47
JackFisherDev <yevgenrybalchenko@gmail.com>
2020-07-22 12:48:30 +0400

    use Tabs Chrome API to get tab url

--- js/content.js
+++
@@ -1,3 +0,0 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  sendResponse({ href: location.href });
-});
--- js/popup.js
+++ js/popup.js
@@ -8,23 +8,17 @@ function disableBtn() {
 
 function generateQR() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(
-      tabs[0].id,
-      { action: "give me your location.href" },
-      function (response) {
-        disableBtn();
-        qrPlaceholder.style.display = "none";
+    disableBtn();
+    qrPlaceholder.style.display = "none";
 
-        var qrcode = new QRCode(document.getElementById("qr"), {
-          text: response.href,
-          width: 128,
-          height: 128,
-          colorDark: "#000000",
-          colorLight: "#ffffff",
-          correctLevel: QRCode.CorrectLevel.H,
-        });
-      }
-    );
+    var qrcode = new QRCode(document.getElementById("qr"), {
+      text: tabs[0].url,
+      width: 128,
+      height: 128,
+      colorDark: "#000000",
+      colorLight: "#ffffff",
+      correctLevel: QRCode.CorrectLevel.H,
+    });
   });
 }
 
